a5492bf83c265f049b938ab5d93ad071227775f8
Juan Maldonado <maldonadojsm@gmail.com>
2019-10-09 01:59:28 -0500

    modifying slupipe.py

--- src/slupipe.py
+++ src/slupipe.py
@@ -14,6 +14,7 @@ import controller as cn
 import json
 import sys
 import os
+import argparse
 from subprocess import call, check_output
 
 
@@ -41,7 +42,40 @@ def main():
     """
     Main method will process differing executions of program depending if the user has provided a config.json file
     """
-    # DEFAULT MODE
+    parser = argparse.ArgumentParser(description="Process Custom Variant Caller Configs")
+
+    parser.add_argument('slupipe_conf',metavar='<config.json>', action='store', type=str, help='SLUPipe Config File')
+    parser.add_argument('variant_one', nargs='?', help='Variant Caller Config 1', default='')
+    parser.add_argument('variant_two', nargs='?', help='Variant Caller Config 2',default='')
+    parser.add_argument('variant_three',nargs='?', help='Variant Caller Config 3',default='')
+    parser.add_argument('variant_four', nargs='?',help='Variant Caller Config 4',default='')
+    parser.add_argument('variant_five', nargs='?', help='Variant Caller Config 5',default='')
+
+    args = parser.parse_args()
+
+    variant_caller = ["muse.json", "mutect.json", "varscan.json", "sniper.json"]
+    if args.variant_one:
+        if args.variant_one in variant_caller:
+            print(os.path.abspath(args.variant_one))
+    if args.variant_two:
+        if args.variant_two in variant_caller:
+            print(os.path.abspath(args.variant_two))
+
+    if args.variant_three:
+        if args.variant_three in variant_caller:
+            print(os.path.abspath(args.variant_three))
+
+    if args.variant_four:
+        if args.variant_four in variant_caller:
+            print(os.path.abspath(args.variant_four))
+
+    if args.variant_five:
+        if args.variant_five in variant_caller:
+            print(os.path.abspath(args.variant_five))
+
+
+
+    #DEFAULT MODE
     if len(sys.argv) == 2:
 
         # User has indicated to check for updates
@@ -64,6 +98,10 @@ def main():
             with open(sys.argv[1], 'r') as file:
                 config_dict = json.load(file)
 
+
+            args = parser.parse_args()
+
+            print(args)
 
             print(os.path.abspath(sys.argv[2]))
             slu_pipe = cn.Controller(1, config_dict, os.path.abspath(sys.argv[2]))
